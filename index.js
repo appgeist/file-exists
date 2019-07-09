e14ed2e1e099aab86a65d14ea30fc877be905e49
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2019-07-09 12:05:51 +0300

    Add jsdoc

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ const { access, constants } = require('fs');
 
 const { F_OK } = constants;
 
+/**
+ * Check if a file exists
+ *
+ * @param {string} path Filename to check
+ * @returns {Promise<boolean>} A Promise that resolves with true or false
+ */
 module.exports = path => new Promise(resolve => access(path, F_OK, err => resolve(!err)));
